2b0be301f5b5d937bc37d018fc6d5a456bb2ab12
ceyda <elmsceyda@gmail.com>
2024-01-23 15:53:47 +0300

    Unit Of Work eklendi. Core katmanı tamamlandı

---
+++ NLayer.Core/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.UnitOfWorks
+{
+    public interface IUnitOfWork
+    {
+        //veri tabanına yapılacak işlemleri toplu bir şekilde tek bir transaction üzerinden yönetmemize izin verir
+        Task CommitAsync();
+        void Commit();
+    }
+}
