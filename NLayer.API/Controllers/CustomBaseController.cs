735fc130b7024d37a889ac212ee0131cc902b042
ceyda <elmsceyda@gmail.com>
2024-01-24 12:48:48 +0300

    Product Controller is added

---
+++ NLayer.API/Controllers/CustomBaseController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLayer.Core.DTOs;
+
+namespace NLayer.API.Controllers
+{
+    //[Route("api/[controller]")]
+    //[ApiController]
+    public class CustomBaseController : ControllerBase
+    {
+        [NonAction] //bu bir endpoint değil o yüzden no action koyuyorum.Swaggerda çıkmayacak
+        public IActionResult CreateActionResult<T>(ApiResponseDTO<T> apiResponse)
+        {
+            if (apiResponse.StatusCode == 204)//no content 
+                return new ObjectResult(null)
+                {
+                    StatusCode = apiResponse.StatusCode
+                };
+            return new ObjectResult(apiResponse)
+            {
+                StatusCode = apiResponse.StatusCode
+            };
+        }
+    }
+}
---
+++ NLayer.API/Controllers/ProductController.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NLayer.Core.DTOs;
+using NLayer.Core.Models;
+using NLayer.Core.Services;
+
+namespace NLayer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : CustomBaseController
+    {
+        private readonly IMapper _mapper;
+        private readonly IService<Product> _service;//repo katmanını bilmeyecek service katmanından alacak
+
+        public ProductController(IMapper mapper, IService<Product> service)
+        {
+            _mapper = mapper;
+            _service = service;
+        }
+        //api/products
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            var products = _service.GetAllAsync();
+            var productsDTO= _mapper.Map<List<ProductDTO>>(products); //mapping gibi işlemlerin her zaman service katmanına yani business logic tarafında yapılmasıdır. bu düzeltilecek
+            return CreateActionResult(ApiResponseDTO<List<ProductDTO>>.Success(200,productsDTO));
+        }
+
+        //api/product/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var product = _service.GetByIdAsync(id);
+            var productDTO = _mapper.Map<ProductDTO>(product); //mapping gibi işlemlerin her zaman service katmanına yani business logic tarafında yapılmasıdır. bu düzeltilecek
+            return CreateActionResult(ApiResponseDTO<ProductDTO>.Success(200, productDTO));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(ProductDTO productDto)
+        {
+            var product = await _service.AddAsync(_mapper.Map<Product>(productDto));
+            var productDTO = _mapper.Map<ProductDTO>(product); //productı alıp geri dtoya dönüştürdük
+            return CreateActionResult(ApiResponseDTO<ProductDTO>.Success(201, productDTO)); //201-created
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(ProductUpdateDTO productDto)
+        {
+            await _service.UpdateAsync(_mapper.Map<Product>(productDto));
+            return CreateActionResult(ApiResponseDTO<NoContentDTO>.Success(201)); //201-created
+        }
+
+        //delete api/product/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Remove(int id)
+        {
+            var product = await _service.GetByIdAsync(id);
+            await _service.RemoveAsync(product);
+            return CreateActionResult(ApiResponseDTO<NoContentDTO>.Success(204));
+        }
+    }
+}
