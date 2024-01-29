6990af6f3fa322ee79df6fc987f9a72ee4a81eaf
ceyda <elmsceyda@gmail.com>
2024-01-29 14:37:39 +0300

    MVC Products controller is added

---
+++ NLayer.Web/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NLayer.Core.Services;
+
+namespace NLayer.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var customResponse = await _productService.GetProductsWithCategory();
+            return View(customResponse.Data);//productsları dönüyorum. direkt veri lazım bize ekranda. Aslında burda service katmaında custom response dönmeme gerek yok ama böyle de olur.
+        }
+    }
+}
--- NLayer.Web/Program.cs
+++ NLayer.Web/Program.cs
@@ -1,7 +1,28 @@
+using Autofac.Extensions.DependencyInjection;
+using Autofac;
+using NLayer.Web.Modules;
+using Microsoft.EntityFrameworkCore;
+using NLayer.Repository;
+using NLayer.Service.Mapping;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddAutoMapper(typeof(MapProfile));
+builder.Services.AddDbContext<AppDbContext>(x =>
+{
+    x.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection"), option =>
+    {
+        option.MigrationsAssembly(Assembly.GetAssembly(typeof(AppDbContext)).GetName().Name);
+        //direkt assembly adýný yazmak yerine olduðu yeri bu þekil tanýmladýk daha dinamik. AppDbcontextin olduðu assmeblyi alýyor.
+        //NLayer.Repository
+    });
+    //App dbcontextimin olduðu assemblyi tanýtmam lazým.repository katmanýnda
+});
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder => containerBuilder.RegisterModule(new RepoServiceModule()));
 
 var app = builder.Build();
 
